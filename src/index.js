f68cec7cf6d6f7f2dae223e029661e68cc61c2a8
Jack Cooper <jack@acooper.net>
2021-01-19 18:57:40 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,48 +1,51 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
-let mainWindow;
-
-function createWindow () {
-  mainWindow = new BrowserWindow({
+const { app, BrowserWindow , autoUpdater} = require('electron');
+const path = require('path');
+require('update-electron-app')({
+  repo:'jackcooper04/electron-publish-example',
+  updateInterval:'5 min',
+  logger:require('electron-log'),
+  
+})
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+//HelloWorld
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
   });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-}
-mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
-app.on('ready', () => {
-  createWindow();
-});
 
-app.on('window-all-closed', function () {
+  // and load the index.html of the app.
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', function () {
-  if (mainWindow === null) {
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-});
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-  autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-  });
-  ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-  });
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
